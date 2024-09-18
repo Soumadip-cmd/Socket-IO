0ee7b9af7329a5fa8db67ff77100bf5fb811bebf
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-09-18 13:45:41 +0530

    admin ui

--- server/index.js
+++ server/index.js
@@ -1,18 +1,24 @@
-
+const { instrument } = require("@socket.io/admin-ui");
 
 const io = require("socket.io")(8080, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://admin.socket.io"],
+    methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 
 
-io.on('connect',socket=>{
-  console.log(socket.id)
-  socket.on('msg-event',(cb)=>{
-    // console.log(msg)
-    cb('i gotcha')
-  })
+io.on("connect", (socket) => {
+  console.log("Socket connected:", socket.id);
 
-  socket.emit('server-msg',"Server connected");
-})
+  socket.on("msg-event", (cb) => {
+    // Callback function to acknowledge the message
+    cb("i gotcha");
+  });
+
+  socket.emit("server-msg", "Server connected");
+});
+
+// Setting up admin-ui instrumentation
+instrument(io, { auth: false });
